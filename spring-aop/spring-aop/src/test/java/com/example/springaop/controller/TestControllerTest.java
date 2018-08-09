0f5cbc92b3ab1cc7ea2ef379077a77520c67b6f6
zk <zkzmzk@163.com>
2018-08-09 11:44:26 +0800

    aop 日志记录 测试用例

---
+++ spring-aop/spring-aop/src/test/java/com/example/springaop/controller/TestControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.springaop.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by zk on 2018/8/9.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void queryTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/query"))
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    public void bodyTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/body").content("test"))
+                .andExpect(status().isOk());
+    }
+}
